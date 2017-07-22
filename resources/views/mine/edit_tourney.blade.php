2e93269f143dddb793c9bc58989c1a1c87b4345d
Eric Miller <eric.miller2129@gmail.com>
2017-07-22 01:15:40 -0400

    Added purse, fixed an edit tourney error

--- resources/views/mine/edit_tourney.blade.php
+++ resources/views/mine/edit_tourney.blade.php
@@ -61,6 +61,15 @@
                             </span>
                         @endif
                     </div>
+                    <div class="form-input{{ $errors->has('purse') ? ' has-error' : '' }}{{ $has_societies ? '' : ' is-disabled' }}">
+                        <label for="purse">Purse Amount:</label>
+                        <input type="text" name="purse" id="purse"{{ $has_societies ? '' : ' disabled' }} value="{{ $tourney->purse }}">
+                        @if ($errors->has('purse'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('purse') }}</strong>
+                            </span>
+                        @endif
+                    </div>
                     <div class="form-input{{ $errors->has('entry_fee') ? ' has-error' : '' }}{{ $has_societies ? '' : ' is-disabled' }}">
                         <label for="entry_fee">Entry Fee:</label>
                         <input type="text" name="entry_fee" id="entry_fee"{{ $has_societies ? '' : ' disabled' }} value="{{ $tourney->entry_fee }}">
