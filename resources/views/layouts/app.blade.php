2e531a459c067bb3f0cd8976c077adece9788802
Eric <emiller@tax-guard.com>
2017-07-24 11:30:15 -0600

    Added description meta

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="description" content="TGCSocieties is an online manager for The Golf Club 2 society presidents. Create tournaments, and share your leaderboard anywhere you'd like!">
 
         <title>TGC Societies</title>
 
